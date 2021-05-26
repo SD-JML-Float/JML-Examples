156982e1846131dba6ffd93114e6089e4e7a780f
Robert Moore <rmoore98@knights.ucf.edu>
2021-05-26 10:00:44 -0400

    some trig test cases

--- java_lang_Math/trig/Sine.java
+++ java_lang_Math/trig/Sine.java
@@ -1,16 +1,15 @@
 // written by Robert Moore 5/23/21
 
-public class Cosine {
-
+public class Sine {
   // requires (! Double.isNaN(a)) && (! Double.isInfinite(a));
   //@ requires Math.isFinite(a);
   //@ ensures \result >= -1.0 && \result <= 1.0;
   static double inBounds(double a){
-    return Math.cos(a);
+    return Math.sin(a);
   }
 
   //@ ensures \result == 1.0;
-  static double cosZero(){
-    return Math.cos(0.0);
+  static double sinZero(){
+    return Math.sin(0.0);
   }
 }
