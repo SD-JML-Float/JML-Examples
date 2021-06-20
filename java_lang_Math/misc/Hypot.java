6d6aaafb742c31307c2f204891a67fafd44c3974
garytho <garyhoppenworth@gmail.com>
2021-06-20 18:54:25 -0400

    Improved my test cases for my assigned Math methods

--- java_lang_Math/misc/Hypot.java
+++ java_lang_Math/misc/Hypot.java
@@ -3,31 +3,64 @@ import java.lang.Math;
 public class Hypot
 {
 
-// //THE FOLLOWING THREE EXAMPLES SHOULD BE VALIDATED BY OPENJML
-
-// // the square root of b^2 equals b
 //@ requires a == 0.0;
-//@ ensures \result == b;
-public static double hypot_test_1(double a, double b)
+public static void hypot_test_1(double a, double b)
 {
 	double c = Math.hypot(a, b);
-	return c;
+	//@ assert c == b;
 }
 
 //@ requires a == 0.0 && b == 0.0;
-//@ ensures \result == 0.0;
-public static double hypot_test_2(double a, double b)
+public static void hypot_test_2(double a, double b)
 {
         double c = Math.hypot(a, b);
-        return c;
+	//@ assert c == 0.0;
 }
 
 //@ requires a == 1.0 && b == 0.0;
-//@ ensures \result == 1.0;
-public static double hypot_test_3(double a, double b)
+public static void hypot_test_3(double a, double b)
 {
         double c = Math.hypot(a, b);
-        return c;
+        //@ assert a == 1.0;
+}
+
+
+// If either argument is infinite, the result is positive infinity
+public static void inf_test(double a, double b)
+{
+	double c = Math.hypot(Double.POSITIVE_INFINITY, Double.POSITIVE_INFINITY);
+	//@ assert Double.isInfinite(c);
+
+	c = Math.hypot(a, Double.POSITIVE_INFINITY);
+	//@ assert Double.isInfinite(c);
+	
+	c = Math.hypot(Double.POSITIVE_INFINITY, b);
+	//@ assert Double.isInfinite(c);
+	
+	c = Math.hypot(a, Double.NEGATIVE_INFINITY);
+	//@ assert Double.isInfinite(c);
+	
+	c = Math.hypot(Double.NEGATIVE_INFINITY, b);
+	//@ assert Double.isInfinite(c);
+	
+	c = Math.hypot(a, b);
+	//@ assert (Double.isInfinite(a) || Double.isInfinite(b)) ==> Double.isInfinite(c);
+}
+
+// If either argument is NaN and no arguments are infinite, the result is NaN
+public static void nan_test(double a, double b)
+{
+	double c = Math.hypot(Double.NaN, Double.NaN);
+	//@ assert Double.isNaN(c);
+	
+	c = Math.hypot(a, Double.NaN);
+	//@ assert !Double.isInfinite(a) ==> Double.isNaN(c);
+	
+	c = Math.hypot(Double.NaN, b);
+	//@ assert !Double.isInfinite(b) ==> Double.isNaN(c);
+	
+	c = Math.hypot(a, b);
+	//@ assert ( (Double.isNaN(a) || Double.isNaN(b)) &&  !Double.isInfinite(a) && !Double.isInfinite(b) )  ==> Double.isNaN(c);
 }
 
 }
